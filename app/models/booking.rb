e1daf0a68bb1109aaf450384440e16fe03fd90c5
Marc Bayle <marc.scl.bayle@gmail.com>
2023-04-22 09:30:55 +0200

    ok

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,10 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :cut
   belongs_to :user
 
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, uniqueness: true
+  validates :description, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
-  validates :email, presence: true, uniqueness: true
 end
