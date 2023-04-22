5bacd6ea56943df57e74715faab46790cdb92399
Marc Bayle <marc.scl.bayle@gmail.com>
2023-04-22 13:00:35 +0200

    characteres add

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :cut
   belongs_to :user
 
-  validates :description, presence: true
+  validates :description, presence: true, :length => { :maximum => 200 }
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  # private
+
+  # def not_too_many_lines
+  #   if self.description.lines.count > 10
+  #     errors.add :description, "has too many lines"
+  #   end
+  # end
+
 end
