0e2c2b44f2c0793bc78d0b494729432e1a5074a4
Marc Bayle <marc.scl.bayle@gmail.com>
2023-05-02 11:23:12 +0200

    flatpickr brrr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,41 +1,14 @@
-import { Controller } from '@hotwired/stimulus';
-import flatpickr from 'flatpickr';
-// import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    const config = {
-          minDate: "today",
-          altInput: true,
-          mode: 'range',
-          dateFormat: "Y-m-d",
-          onClose: function(_selectedDates, _dateStr, fp) => {
-            if (fp.selectedDates.length === 1) {
-              const startDate = fp.formatDate(fp.selectedDates[0], 'Y-m-d');
-              document.getElementById('start_time').value = startDate;
-            }
-          }
-    }
-    console.log(startDate);
-    flatpickr('#start_time', config);
-  };
+    console.log("connected", this.element);
+
+    flatpickr("#booking_start_time", {
+      enableTime: true,
+      dateFormat: "F j, Y h:i K",
+    });
+  }
 }
-
-
-  //   const config = {
-  //     minDate: "today",
-  //     altInput: true,
-  //     mode: 'range',
-  //     weekNumbers: true,
-  //     altFormat: "F j, Y",
-  //     dateFormat: "Y-m-d",
-  //     disable: "today",
-  //     plugins: [new rangePlugin({ input: this.endTimeTarget })],
-  //     onClose: (_, __, fp) => {
-  //       if (fp.selectedDates.length === 2) {
-  //         const startDate = fp.formatDate(fp.selectedDates[0], 'Y-m-d');
-  //         const endDate = fp.formatDate(fp.selectedDates[1], 'Y-m-d');
-  //             // document.getElementById('end_time').value = endDate;
-                // document.getElementById('start_time').value = startDate;
-    //       }
-  //     },
-  //   };
